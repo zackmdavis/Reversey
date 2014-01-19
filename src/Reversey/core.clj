9a493cd13543aeb7f0e91e7c3de59046e0fb791a
Zack M. Davis <code@zackmdavis.net>
2014-01-19 13:15:35 -0800

    displacement and flipping mechanics

--- src/Reversey/core.clj
+++ src/Reversey/core.clj
@@ -31,3 +31,37 @@
     (if (contains? colors @square)
       (swap! square opposing_color)
       nil)))
+
+(defn displacement [position1 position2]
+  [(- (row position2) (row position1))
+   (- (col position2) (col position1))])
+
+(defn displaced_by [position displacement]
+  [(+ (row position) (row displacement))
+   (+ (col position) (col displacement))])
+
+(defn normalized [displacement]
+  (if (or (= (row displacement) 0)
+          (= (col displacement) 0)
+          (= (Math/abs (row displacement)) (Math/abs (col displacement))))
+    (let [magnitude (max (row displacement) (col displacement))]
+      [(/ (row displacement) magnitude)
+       (/ (col displacement) magnitude)])
+    nil))
+
+;; This is not very elegant and should probably be rewritten
+(defn positions_between_positions [position1 position2]
+  (let [direction
+        (normalized (displacement position1 position2))]
+    (if direction
+      (loop [positions [position1]]
+        (if (= (last positions) position2)
+          (take (- (count positions) 2) (rest positions)) ;; wtf
+          (recur (conj positions
+                       (displaced_by (last positions)
+                                     direction)))))
+      [])))
+
+(defn flip_between_positions! [board position1 position2]
+  (doseq [p (positions_between_positions position1 position2)]
+    (flip! board p)))
--- test/Reversey/test/core.clj
+++ test/Reversey/test/core.clj
@@ -14,3 +14,40 @@
   (flip! test_board [2 2])
   (is (= (second colors)
          (deref (lookup test_board [2 2])))))
+
+(deftest test_displacement
+  (is (= (displacement [1 1] [1 6]) [0 5]))
+  (is (= (displacement [1 1] [4 1]) [3 0]))
+  (is (= (displacement [1 1] [4 4]) [3 3]))
+  (is (= (displacement [1 1] [3 6]) [2 5]))
+  (is (= (displacement [3 6] [1 1]) [-2 -5])))
+
+(deftest test_displaced_by
+  (is (= (displaced_by [2 3] [1 1]) [3 4]))
+  (is (= (displaced_by [4 4] [-1 2]) [3 6])))
+
+(deftest test_normalized_displacement
+  (is (= (normalized [4 0]) [1 0]))
+  (is (= (normalized [0 5]) [0 1]))
+  (is (= (normalized [3 -3]) [1 -1]))
+  (is (= (normalized [1 -2]) nil)))
+
+(deftest test_positions_between
+  (is (= (positions_between_positions [0 0] [0 3])
+         [[0 1] [0 2]]))
+  (is (= (positions_between_positions [1 2] [3 4])
+         [[2 3]]))
+  (is (= (positions_between_positions [0 0] [1 2])
+         [])))
+
+(deftest test_flip_between
+  (let [test_board (make_board 8)
+        positions [[1 0] [2 0] [3 0]]]
+    (doseq [position positions]
+      (place_disc! test_board position (first colors)))
+    (flip_between_positions! test_board [0 0] [4 0])
+    (let [resulting_colors
+          (for [position positions]
+            (deref (lookup test_board position)))]
+      (is (every? (fn [color] (= color (second colors)))
+                  resulting_colors)))))
