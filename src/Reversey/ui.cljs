bd23dc81e845486707dac06b1240beea81ff2610
Zack M. Davis <code@zackmdavis.net>
2014-01-26 13:04:36 -0800

    Hello HTML5 Canvas World

--- src/Reversey/ui.cljs
+++ src/Reversey/ui.cljs
@@ -7,9 +7,23 @@
 (r/place_disc! board [4 3] :black)
 (r/place_disc! board [4 4] :white)
 
-(doseq [row board]
-  (let [symbols (map #(cond (= :white (deref %)) " &#9675; "
-                            (= :black (deref %)) " &#9679; "
-                            :else " _ ")
-                     row)]
-    (.write js/document (str (str symbols) " <br>"))))
+(defn draw_line [context width height start_row start_col end_row end_col]
+  (let [unit (/ width 8)]
+    (.beginPath context)
+    (.moveTo context (* start_col unit) (* start_row unit))
+    (.lineTo context (* end_col unit) (* end_row unit))
+    (.stroke context)))
+
+(defn draw_board []
+  (let [canvas (.getElementById js/document "canvas")
+        width (.getAttribute canvas "width")
+        height (.getAttribute canvas "height")
+        context (.getContext canvas "2d")]
+    (set! (.-fillStyle context) "green")
+    (.fillRect context 0 0 width height)
+    (doseq [row (range 1 8)]
+      (draw_line context width height row 0 row 8))
+    (doseq [col (range 1 8)]
+      (draw_line context width height 0 col 8 col))))
+
+(draw_board)
