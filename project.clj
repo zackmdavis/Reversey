d9bc373800b4c97275711cdd4ccca850fc5d2dab
Zack M. Davis <code@zackmdavis.net>
2014-01-25 22:13:02 -0800

    converts project to ClojureScript
    Somehow I had harbored some hope that targeting Clojure and
    ClojureScript with some of the same source files would just work.

--- project.clj
+++ project.clj
@@ -3,4 +3,13 @@
   :url "http://github.com/zackmdavis/Reversey"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :plugins [[lein-cljsbuild "0.2.7"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojurescript "0.0-1450"]]
+  :source-paths ["src"]
+  :cljsbuild {:builds 
+              [{:source-path "src"
+               :compiler 
+                {:output-to "resources/reversey.js"
+                 :optimizations :whitespace
+                 :pretty-print true}}]})
