387af0f786bb65e19262e0128a75d8b107071bfa
Zack M. Davis <code@zackmdavis.net>
2014-02-01 10:35:26 -0800

    removes unused flipping approach
    (Also, since "in_bounds" [sic] returns a boolean, it should, by
    convention, get an interrogative name.)

--- src/Reversey/core.clj
+++ src/Reversey/core.clj
@@ -44,42 +44,18 @@
 (defn abs [x]
   (Math/abs x))
 
-(defn magnitude [displacement]
-  (apply max (map abs displacement)))
-
-(defn normalized [displacement]
-  (if (or (= (row displacement) 0)
-          (= (col displacement) 0)
-          (= (abs (row displacement)) (abs (col displacement))))
-    (vec (map (fn [component]
-                (/ component (magnitude displacement)))
-              displacement))))
-
-(defn positions_between_positions [position1 position2]
-  (let [difference (displacement position1 position2)
-        direction (normalized difference)
-        between (magnitude difference)]
-    (if direction
-      (for [step (range 1 between)]
-        (displaced_by_times position1 direction step))
-      [])))
-
-(defn flip_between_positions! [board position1 position2]
-  (doseq [p (positions_between_positions position1 position2)]
-    (flip! board p)))
-
 (def directions
   (remove #(= [0 0] %)
           (for [i (range -1 2)
                 j (range -1 2)]
             [i j])))
 
-(defn in_bounds [board_size position]
+(defn in_bounds? [board_size position]
   (every? #(and (>= % 0) (< % board_size)) 
           position))
 
 (defn positions_in_direction [board_size position direction]
-  (take-while #(in_bounds board_size %)
+  (take-while #(in_bounds? board_size %)
               (for [step (range 1 board_size)]
                 (displaced_by_times position direction step))))
 
--- test/Reversey/test/core.clj
+++ test/Reversey/test/core.clj
@@ -26,36 +26,10 @@
   (is (= (displaced_by [2 3] [1 1]) [3 4]))
   (is (= (displaced_by [4 4] [-1 2]) [3 6])))
 
-(deftest test_normalized_displacement
-  (is (= (normalized [4 0]) [1 0]))
-  (is (= (normalized [0 5]) [0 1]))
-  (is (= (normalized [3 -3]) [1 -1]))
-  (is (= (normalized [1 -2]) nil)))
-
-(deftest test_positions_between
-  (is (= (positions_between_positions [0 0] [0 3])
-         [[0 1] [0 2]]))
-  (is (= (positions_between_positions [1 2] [3 4])
-         [[2 3]]))
-  (is (= (positions_between_positions [0 0] [1 2])
-         [])))
-
-(deftest test_flip_between
-  (let [test_board (make_board 8)
-        positions [[1 0] [2 0] [3 0]]]
-    (doseq [position positions]
-      (place_disc! test_board position (first colors)))
-    (flip_between_positions! test_board [0 0] [4 0])
-    (let [resulting_colors
-          (for [position positions]
-            (deref (lookup test_board position)))]
-      (is (every? (fn [color] (= color (second colors)))
-                  resulting_colors)))))
-
-(deftest test_in_bounds
-  (is (= (in_bounds 8 [7 7]) true))
-  (is (= (in_bounds 8 [1 0]) true))
-  (is (= (in_bounds 8 [-1 9]) false)))
+(deftest test_in_bounds?
+  (is (= (in_bounds? 8 [7 7]) true))
+  (is (= (in_bounds? 8 [1 0]) true))
+  (is (= (in_bounds? 8 [-1 9]) false)))
 
 (deftest test_positions_in_direction
   (is (positions_in_direction 8 [7 2] [-1 1])
